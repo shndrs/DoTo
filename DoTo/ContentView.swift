9ea38628d4ece15ff0fea8545025b0e77476bec9
shndrs <sahandraeisi1994@gmail.com>
2020-11-29 10:45:30 +0330

    Initial commit

---
+++ DoTo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DoTo
+//
+//  Created by Sahand Raeisi on 11/29/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
