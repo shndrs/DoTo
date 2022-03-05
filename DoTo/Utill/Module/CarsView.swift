d4accc1f0cdbcdd12ead0c31305654093c06848c
Sahand Raeisi <sahandraeisi1994@gmail.com>
2022-03-05 11:20:00 +0330

    Updated with navigation view

--- DoTo/Utill/Module/CarsView.swift
+++ DoTo/Utill/Module/CarsView.swift
@@ -10,21 +10,21 @@ import SwiftUI
 struct CarsView: View {
     
     var body: some View {
-        
-        List(0 ..< 5) { item in
-            HStack {
-                Image("my location")
-                    .resizable()
-                    .frame(width: 25, height: 25)
-                VStack(alignment: .leading) {
-                    Text("Ferrari")
-                    Text("FXXK Evo")
-                        .foregroundColor(Color.red)
-                        
+        NavigationView {
+            List(0 ..< 5) { item in
+                HStack {
+                    Image("my location")
+                        .resizable()
+                        .frame(width: 25, height: 25)
+                    VStack(alignment: .leading) {
+                        Text("Ferrari")
+                        Text("FXXK Evo")
+                            .foregroundColor(Color.red)
+                            
+                    }
                 }
             }
-        }
-        
+        }.navigationTitle(Text("Scuderia Ferrari"))
     }
     
 }
